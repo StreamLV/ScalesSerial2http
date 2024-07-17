84110b933e70d0b62512c0e4fa0f3b1c764b6bed
StreamLV <l.vitaliy.m@gmail.com>
2024-07-17 23:47:57 +0300

    feat (win serv files)

---
+++ include/build/service-stop.bat
@@ -0,0 +1,2 @@
+@echo off
+.\scales_serial_2_http-service.exe stop .\scales_serial_2_http-service.xml
---
+++ include/build/service-uninstall.bat
@@ -0,0 +1,2 @@
+@echo off
+.\scales_serial_2_http-service.exe uninstall .\scales_serial_2_http-service.xml
